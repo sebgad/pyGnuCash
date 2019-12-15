8b0c8f6fd172f3d3576b38d9c961dac05ef1142c
sebigad <Sebastian.Gade@gmail.com>
2019-12-15 12:30:09 +0100

    changed Structure, Comments

--- GnucashAcc.py
+++ GnucashAcc.py
@@ -17,8 +17,17 @@ pd.set_option('display.max_columns', 20)
 path = r"/home/sebastian/Dokumente/Bank/GnuCashSoftware/Privatvermögen.db.sql.gnucash"
 
 class gnuCashAccess():
-    def __init__(self, path):
-        self.objCon = sqlite3.connect(path)
+    '''
+    Access GnuCash created SQL-Lite File to get Splits and Accounts
+    '''
+    def __init__(self, strPath):
+        '''
+        Initialize GnuCash - Object
+        
+        Parameter:
+            strPath: Local Path to GnuCash-File
+        '''
+        self.objCon = sqlite3.connect(strPath)
         sqlString = ("SELECT * FROM accounts;")
         
         self.dfAccounts = pd.read_sql(sqlString, self.objCon)
@@ -32,38 +41,67 @@ class gnuCashAccess():
                      "FROM transactions as t "
                      "INNER JOIN splits ON t.guid=splits.tx_guid;")
 
-        self.Splits = pd.read_sql(sqlString, 
+        self.dfSplits = pd.read_sql(sqlString, 
                              self.objCon, 
                              parse_dates=['enter_date', "post_date"])
 
-        self.Splits['price'] = self.Splits.value_num / self.Splits.value_denom
-        self.Splits['post_month'] = self.Splits.post_date.dt.strftime("%Y-%m")
-        
-#    def getSplitsGrouped(self, ParentAccountGuid, GroupBy, Agg, startDate=None, endDate=None):
-#        mainGrp = tree.childLookup.get(ParentAccountGuid)
-#        
-#        idxDateRng = np.ones((len(self.Splits),)).astype(np.bool)
-#        
-#        if startDate is not None:
-#            idxDateRng = (idxDateRng) & (self.Splits['post_date']>=startDate)
-#        
-#        if endDate is not None:
-#            idxDateRng = (idxDateRng) & (self.Splits['post_date']<=endDate)
-#        
-#        dfgrouped = pd.DataFrame(index=self.Splits[idxDateRng]['post_month'].unique())
-#        
-#        for member in mainGrp:
-#            children = tree.findChildren(member.guid)
-#            if member['placeholder'] == 0:
-#                children.append(member.guid)
-#            
-#            idx = (self.Splits['account_guid'].isin(children)) & (idxDateRng)
-#            
-#            dfgrouped[member['name']] = self.Splits[idx].groupby(by=GroupBy).agg(Agg)['price']
-#        
-#        return dfgrouped
+        self.dfSplits['price'] = self.dfSplits.value_num / self.dfSplits.value_denom
+        self.dfSplits['post_month'] = self.dfSplits.post_date.dt.strftime("%Y-%m")
     
-    def TOP(self, df, TOP=8, axis=1):
+    def getSplits(self, ParentID, startDate=None, endDate=None):
+        '''
+        get all Splits, grouped monthly, in a specified Date Range from a specified
+        Parent-Account
+        
+        Parameter:
+            ParentID: String of the Parent-Account ID
+            startDate: Datetime object of the Start-Date
+            endDate: Datetime object of the End-Date
+        
+        Return:
+            df_grouped: DataFrame Object where index is like "%Y-%m" and 
+            columns are direct subaccounts of the specified ParentID
+        '''
+        lst_directChildren = self.objAccTree.dictChildLookup.get(ParentID)
+        idxDateRng = np.ones((len(self.dfSplits),)).astype(np.bool)
+        
+        if startDate is not None:
+            idxDateRng = (idxDateRng) & (self.dfSplits['post_date']>=startDate)
+        
+        if endDate is not None:
+            idxDateRng = (idxDateRng) & (self.dfSplits['post_date']<=endDate)
+        
+        df_month_unique = pd.DataFrame(index=self.dfSplits[idxDateRng]['post_month'].sort_values().unique())
+        df_grouped = pd.DataFrame(index=df_month_unique.index)
+        
+        for childAcc in lst_directChildren:
+            lst_children = self.objAccTree.searchTree(childAcc['guid'], 'guid', [])
+            if childAcc['placeholder'] == 0:
+                lst_children.append(childAcc['guid'])
+            
+            idx = (self.dfSplits['account_guid'].isin(lst_children)) & (idxDateRng)
+            
+            df_grouped[childAcc['name']] = self.dfSplits[idx].groupby(by='post_month').agg('sum')['price']
+        
+        return df_grouped
+    
+    def getTOP(self, df, TOP=8, axis=1):
+        '''
+        Reduce the Amount of Accounts by concatenating all Accounts which
+        are higher than specified by TOP. 
+        
+        It firstly sum up all Accounts and order it accordingly. Accounts which
+        have a lower Summation than the TOP-Accounts will be summed up and 
+        named with "others"
+        
+        Parameter:
+            df: DataFrame-Account with Splits
+            TOP: Total Number of Accounts in the results
+            axis: Depending on df the direction to sum up
+        
+        Return:
+            dfTOP: DataFrame with the reduced ammount of Accounts
+        '''
         if axis == 1:
             TOPitems = df.sum(axis=0).sort_values(ascending=False).index
             dfTOP = df[TOPitems[:TOP]].copy()
--- treeSearchTest.py
+++ treeSearchTest.py
@@ -10,8 +10,8 @@ from GnucashAcc import gnuCashAccess
 
 path = r"/home/sebastian/Dokumente/Bank/GnuCashSoftware/Privatvermögen.db.sql.gnucash"
 gnuCash = gnuCashAccess(path)   
-
+t = gnuCash.getSplits('2550a442ee387d7550de6b9e6d61f47a')
 #dfExpAccounts = groupAcc(dfSplits, dfAccounts, '2550a442ee387d7550de6b9e6d61f47a')
-tree = TreeSearch(gnuCash.dfAccounts, 'guid', 'parent_guid')
+#tree = TreeSearch(gnuCash.dfAccounts, 'guid', 'parent_guid')
 
-test = tree.searchTree('5ae680287861ddff39edd8a8d48badcd', 'guid')
+#test = tree.searchTree('5ae680287861ddff39edd8a8d48badcd', 'guid')
